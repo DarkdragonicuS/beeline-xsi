830383f1512317055e11c85c610bfe3fe59e9bec
Nixiris <dark.killer91@hotmail.com>
2021-11-22 17:53:03 +1100

    added event subscription update

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
+from time import sleep
 from flask import Flask,request
 from datetime import datetime,time
-import xmltodict,requests
+import xmltodict,requests,json
+from threading import Thread
+####################   REMOTE DEBUG   #############################
+import debugpy                                                    #
+debugpy.listen(('0.0.0.0', 5678))                                 #  
+debugpy.wait_for_client()                                         #
+###################################################################
 
-listenHost = '172.16.16.120'
+listenHost = '0.0.0.0'
 listenPort = 5000
-debugLogPath = 'c:\\obmen\\log.txt'
+externalUrl = ''
+debugLogPath = ''
 telegramChatIds = []
 telegramToken = ''
+xsiAbonents = []
+xsiSubscribes = dict()
+xsiToken = ''
 endOfWorkTime = time(18)
 startOfWorkTime = time(9)
 weekEnd = [5,6]
@@ -54,4 +65,58 @@ def sendToChat(caller,answerer):
             }
             requests.post(url='https://api.telegram.org/bot'+telegramToken+'/sendMessage',params=params,headers=headers)
 
-app.run(host=listenHost, port=listenPort)
+def subscriptionUpdateJob():
+    while True:
+        sleep(60)
+        for abonent in xsiSubscribes:
+            print('Checking subscription for abonent ' + abonent + '...',end='')
+            if not checkSubscription(xsiSubscribes[abonent]):
+                print('done!')
+                print('Updating subscription...',end='')
+                xsiSubscribes[abonent] = subscribeToEvent(abonent)
+                print('done!')
+            else:
+                print('done!')
+                print('Subscription isn\'t expired.')
+
+def checkSubscription(subscriptionId):
+    headers = {
+        'X-MPBX-API-AUTH-TOKEN':xsiToken,
+        'Accept':'application/json'
+    }
+    params = {
+        'subscriptionId':subscriptionId
+    }
+    response = requests.get(url='https://cloudpbx.beeline.ru/apis/portal/subscription',params=params,headers=headers)
+    if 'expires' in response.json():
+        return True
+    else:
+        return False
+
+def subscribeToEvent(pattern):
+    url = 'https://cloudpbx.beeline.ru/apis/portal/subscription'
+    headers = {
+        'X-MPBX-API-AUTH-TOKEN':xsiToken,
+        'Content-Type':'application/json'
+    }
+    params = {
+        'pattern':pattern,
+        'expires':86400,
+        'subscriptionType':'BASIC_CALL',
+        'url':externalUrl
+    }
+    response = requests.put(url=url,headers=headers,data=json.dumps(params))
+    return response.json()['subscriptionId']
+
+def serverRun():
+    app.run(host=listenHost, port=listenPort)
+
+Thread(target=serverRun).start()
+
+# initial subscription
+for abonent in xsiAbonents:
+    #xsiSubscrubeIds[xsiSubscrubeIds.index(abonent)] = subscribeToEvent(abonent)
+    xsiSubscribes[abonent] = subscribeToEvent(abonent)
+
+Thread(target=subscriptionUpdateJob).start()
+
